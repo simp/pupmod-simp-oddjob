e7d6a9188450a64197ffb40d7465973c1f7b3b2e
Dana Parker <dwatkins-parker@keywcorp.com>
2015-10-28 17:51:57 +0000

    SIMP-578) Add Acceptance Test
    Adding Acceptance Test; updating spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,18 +1,35 @@
 require 'spec_helper'
 
 describe 'oddjob' do
-  let(:facts) {{
-    :fqdn => 'test.host.net',
-    :hardwaremodel => 'x86_64',
-    :processorcount => 4,
-    :interfaces => 'lo',
-    :ipaddress_lo => '127.0.0.1',
-    :operatingsystem => 'RedHat'
-  }}
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) {{
+        :fqdn => 'test.host.net',
+        :hardwaremodel => 'x86_64',
+        :processorcount => 4,
+        :interfaces => 'lo',
+        :ipaddress_lo => '127.0.0.1',
+        :operatingsystem => 'RedHat',  
+        :operatingsystem => 'CentOS' 
+      }}
+      let(:facts) do
+        facts
+      end  
 
-  it { should create_class('oddjob') }
-  it { should compile.with_all_deps }
+      context "on #{os}" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('oddjob') }
 
-  it { should create_package('oddjob') }
-  it { should create_service('oddjobd').that_requires('Package[oddjob]') }
+      context 'base' do 
+        it { is_expected.to create_package('oddjob').with( :ensure => 'latest')}
+#        it { is_expected.to contain_service('oddjob') }
+#        it { is_expected.to contain_service('oddjob').with({
+#          :ensure     => 'running',
+#          :enable     => 'true' } )
+#        }   
+      end
+    end
+  end
 end
+end
+
