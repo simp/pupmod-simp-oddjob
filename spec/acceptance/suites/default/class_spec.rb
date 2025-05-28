fd218005913ad92c5724dd05e1768a8cc9870194
Steven Pritchard <steve@sicura.us>
2025-05-28 10:54:03 -0500

    Clean up for rubocop (#75)
    * Clean up for rubocop
    * Additional cleanup

--- spec/acceptance/suites/default/class_spec.rb
+++ spec/acceptance/suites/default/class_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper_acceptance'
 test_name 'oddjob class'
 
 describe 'oddjob class' do
-  let(:manifest) {
+  let(:manifest) do
     <<-EOS
       include '::oddjob'
     EOS
-  }
+  end
 
   context 'with defaults' do
-    it 'should work with no errors' do
-       apply_manifest(manifest, :catch_failures => true)
+    it 'works with no errors' do
+      apply_manifest(manifest, catch_failures: true)
     end
 
-    it 'should be idempotent' do
-       apply_manifest(manifest, :catch_changes => true)
+    it 'is idempotent' do
+      apply_manifest(manifest, catch_changes: true)
     end
 
     describe package('oddjob') do
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe 'oddjob' do
-  on_supported_os.each do |os, facts|
-    let(:facts) do
-      facts
-    end
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
 
     context "on #{os}" do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to create_class('oddjob') }
 
-      it { is_expected.to create_package('oddjob').with( :ensure => 'installed')}
-      it { is_expected.to contain_service('oddjobd').with({
-          :ensure  => 'running',
-          :enable  => true,
-          :require => 'Package[oddjob]'
-        })
-      }
+      it { is_expected.to create_package('oddjob').with(ensure: 'installed') }
+      it do
+        is_expected.to contain_service('oddjobd')
+          .with(
+            ensure: 'running',
+            enable: true,
+            require: 'Package[oddjob]',
+          )
+      end
     end
   end
 end
